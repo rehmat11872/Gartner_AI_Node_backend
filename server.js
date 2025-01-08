30d76015b2fe4d08f449dc0d74a4c9281806a18f
Muhammad Usman <82944787+Usman11801@users.noreply.github.com>
2025-01-09 03:37:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from './config/db.js'; // Require connectDB
-import swaggerDocs from './docs/swagger.js'; // Require swaggerDocs
+import connectDB from './config/db.js';
+import swaggerDocs from './docs/swagger.js';
 
-import authRoutes from './routes/auth.js'; // Require authRoutes
-import grantRoutes from './routes/grant.js'; // Require grantRoutes
-import questionRoutes from './routes/question.js'; // Require questionRoutes
-import funderRoutes from './routes/funder.js'; // Require funderRoutes
-import organizationRoutes from './routes/organization.js'; // Require organizationRoutes
-
-
+import authRoutes from './routes/auth.js';
+import grantRoutes from './routes/grant.js';
+import questionRoutes from './routes/question.js';
+import funderRoutes from './routes/funder.js';
+import organizationRoutes from './routes/organization.js';
 
 dotenv.config();
 connectDB();
@@ -22,27 +20,18 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// Base API prefix
-// const apiRouter = express.Router();
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/grants', grantRoutes);
 app.use('/api/questions', questionRoutes);
-app.use('/api/questions', questionRoutes);
 app.use('/api/funder', funderRoutes);
 app.use('/api/organization', organizationRoutes);
-
-// Attach the prefixed router to the app
-// app.use('/api', apiRouter);
-
 
 // Home route
 app.get('/', (req, res) => res.send('Welcome to the API'));
 
-// Start the server and initialize Swagger Docs
-app.listen(port, () => {
-  console.log(`Server running on ${process.env.NODE_ENV === 'production' ? 'https://granteater-ai-node-backend.vercel.app' : `http://localhost:${port}`}`);
-  // console.log(`Server running on port ${port}`);
-  swaggerDocs(app, port); // Initialize Swagger
-});
+// Initialize Swagger (only if necessary on startup)
+swaggerDocs(app, port);
+
+// Export the app for Vercel
+export default app;
